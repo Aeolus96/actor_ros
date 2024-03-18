513ff953eb8de131eb57ffdd6c5bc6a5f13ed074
Devson Butani <dbutani@ltu.edu>
2024-03-18 02:47:49 -0400

    test

--- scripts/igvc_scripts/q2.py
+++ scripts/igvc_scripts/q2.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
+import sys
+import time
 
 import actor_ros  # ACTor specific utility functions
 import rospy  # ROS Python API
 
-estop = actor_ros.actor_tools.EStopManager()  # E-Stop Manager instance
-
-actor = actor_ros.scripting_tools.ActorScriptTools()  # ACTor Scripting Tools instance
+# estop = actor_ros.actor_tools.EStopManager()  # E-Stop Manager instance
+# actor = actor_ros.scripting_tools.ActorScriptTools()  # ACTor Scripting Tools instance
 # ^ This starts everything that needs to be up and running for the script
-
 # TODO: Add your code from here
 
-import sys
-import time
 
 print("Hello World!", file=sys.stdout)
 for i in range(10):
